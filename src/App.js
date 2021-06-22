1e047ae3e373324d9a9412ff048eb2a0e0bbb68c
Anand Raja <meetanandns@gmail.com>
2021-06-22 14:23:30 +0000

    react - just begining

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     constructor () {
         super();
         this.state = {
-            string: 'Anand Raja'
+            monster: [
+              {
+                name: 'Frankenstein',
+                id: 'qwe1'
+              },
+                {
+                name: 'Dracula',
+                id: 'qwe2'
+              },
+                {
+                name: 'Zombie',
+                id: 'qwe3'
+              }
+            ]
         };
     }
     render() {
         return (
-            <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Hello { this.state.string }
-                </p>
-                <button onClick={ () => this.setState({ string: 'AR' }) }>Change Text</button>
-            </header>
-            </div>
+          <div className="App">
+            { this.state.monster.map(monster => <h1 key={ monster.id }> { monster.name } </h1> ) }
+          </div>
         );
     }
 }
