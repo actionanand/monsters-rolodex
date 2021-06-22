1a30bdb1c5ae3b294161f857f9d0eb7ea4c65a26
Anand Raja <meetanandns@gmail.com>
2021-06-22 18:59:26 +0000

    first component in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -19,7 +21,9 @@ class App extends Component {
     render() {
         return (
           <div className="App">
-            { this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1> ) }
+            <CardList name="Anand Raja">
+              { this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1> ) }
+            </CardList>
           </div>
         );
     }
